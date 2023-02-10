fc4ad1cfde657ee94e4213cb1d5195dcc37da749
Brian <wamwangimathenge@gmail.com>
2023-02-10 13:28:38 +0300

    12 Passed

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+end
